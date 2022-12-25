62784fff0e5172bfb7f8c008e767a0c083490f50
Shashkist <Shashkist@gmail.com>
2022-12-25 12:09:42 +0200

    Add files via upload

---
+++ FrontEndDevelopment/scrips.js
@@ -0,0 +1,89 @@
+//console.log("hello");
+
+
+let name = 'harry potter';
+const name1 = 'Arya Stark';
+var name2 =  'Frodo';
+
+
+
+let mixed = [1, "Harry" , 2 ];
+
+
+// let nested = [1, mixed, 2, [3,4]];
+
+
+// let pen = {
+//     type : "ballpoint",
+//     color : 'black',
+//     cost : 10
+// };
+
+// console.log(pen);
+
+
+//object.attribute
+//object['attributeName']
+
+
+let emily = {
+    age: 16,
+    school : "DPS",
+    class : "10th std",
+    subjects : ["physics", "chemistry", "math"]
+}
+
+//console.log(emily.subjects);
+
+let arr = [];
+arr = [1,2,3,4];
+
+
+
+// if (arr[0] == 1) {
+//     console.log("correct");
+
+// } else {
+//     console.log("try again");
+// }
+
+//let i = 0;
+// while (i < 5) {
+//     console.log("the number is  : " + i);
+//     i ++;
+// }
+
+// do {
+//     console.log("the number is  : " + i);
+//     i++;
+// }
+// while (i < 1);
+
+// for (i=0; i<5 ; i++) {
+//     console.log("the number is  : " + i);
+// }
+
+
+// // TO check this. 
+//  let games = 'basketball' ;
+//  switch(games) {
+//     case 'basketball' :
+//     console.log("this is basketball");
+//     //break;
+//     case 'football' :
+//         console.log("this is football");
+//         break;
+//     default : 
+//         console.log("this is another game");
+//         break;    
+//  }
+
+
+
+function hello() {
+    console.log("hello");
+}
+
+function product(a,b) {
+    return a*b;
+}
